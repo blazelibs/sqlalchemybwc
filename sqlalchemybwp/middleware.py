343f296ac9993cc5b04f9fcc5a7ec203cebfd532
Matt Lewellyn <guruofgentoo@users.noreply.github.com>
2010-06-14 16:37:37 -0400

    SQLAlchemyContainer and SQLAlchemyApp middleware moved into a plugin. db can be imported from plugstack.sqlalchemybwp.db

---
+++ sqlalchemybwp/middleware.py
@@ -0,0 +1,40 @@
+from pysmvt import settings
+from pysmvt.hierarchy import visitmods
+from paste.registry import StackedObjectProxy
+from sqlalchemy import engine_from_config, MetaData
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+db = StackedObjectProxy(name="db")
+
+class SQLAlchemyContainer(object):
+
+    def __init__(self):
+        self.engine = engine_from_config(dict(settings.db), prefix='')
+        self.meta = MetaData()
+        self.Session = scoped_session(sessionmaker(bind=self.engine))
+
+    def get_session(self):
+        return self.Session()
+
+class SQLAlchemyApp(object):
+    """
+        Creates an Sqlalchemy Engine and Metadata for the application
+
+        Sets up thread-local sessions and cleans them up per request
+    """
+    def __init__(self, application):
+        self.application = application
+        self.container = SQLAlchemyContainer()
+        db.sess = self.container.Session
+        visitmods('model.orm')
+
+    def __call__(self, environ, start_response):
+        def response_cycle_teardown():
+            self.container.Session.remove()
+
+        environ.setdefault('pysmvt.request_setup', [])
+        environ.setdefault('pysmvt.response_cycle_teardown', [])
+        environ['pysmvt.request_setup'].append(request_setup)
+        environ['pysmvt.response_cycle_teardown'].append(response_cycle_teardown)
+        return self.application(environ, start_response)
+            
