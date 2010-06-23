d45ee6f8a71feccebc345b7b327a45d687f8d36b
Randy Syring <randy.syring@level12.io>
2010-06-23 16:13:50 -0400

    moved decorators to decorators.py and adjusted names/usage

--- sqlalchemybwp/lib/helpers.py
+++ sqlalchemybwp/lib/helpers.py
@@ -1,101 +1,28 @@
-from decorator import decorator
-from nose.tools import make_decorator
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm.exc import NoResultFound
 
 from plugstack.sqlalchemy import db
 
-def is_unique_exc(field_name, exc):
+def is_unique_exc(exc):
     if not isinstance(exc, IntegrityError):
         return False
-    return _is_unique_msg(db.engine.dialect.name, field_name, str(exc))
+    return _is_unique_msg(db.engine.dialect.name, str(exc))
 
-def _is_unique_msg(dialect, fieldname, msg):
+def _is_unique_msg(dialect, msg):
     """
         easier unit testing this way
     """
     if dialect == 'postgresql':
-        if 'is not unique' in msg and fieldname in msg:
+        if 'violates unique constraint' in msg:
             return True
     elif dialect == 'mssql':
-        if 'unique index' in msg and fieldname in msg:
-            return True
-        if 'constraint' in msg and fieldname in msg:
+        if 'Cannot insert duplicate key' in msg:
             return True
     elif dialect == 'sqlite':
-        if 'is not unique' in msg and fieldname in msg:
+        if 'is not unique' in msg or 'are not unique' in msg:
             return True
     else:
         raise ValueError('is_unique_exc() does not yet support dialect: %s' % dialect)
     return False
-
-def ignore_unique(*fields):
-    """
-        Wraps the decorated function in a commit/rollback like transwrap() but
-        will also ignore unique exceptions for the fields given.
-    """
-    def decoratorfunc(f):
-        def workerfunc(*args, **kwargs):
-            should_ignore = kwargs.pop('ignore_unique', None)
-            try:
-                retval = f(*args, **kwargs)
-                db.sess.commit()
-                return retval
-            except Exception, e:
-                db.sess.rollback()
-                if not should_ignore:
-                    raise
-                for field in fields:
-                    if is_unique_exc(field, e):
-                        return
-                raise
-        return make_decorator(f)(workerfunc)
-    return decoratorfunc
-
-def cm_ignore_unique(*fields):
-    """
-        like ignore_unique() but makes the decorated function a class method
-    """
-    def decoratorfunc(f):
-        iudf = ignore_unique(*fields)
-        workerfunc = iudf(f)
-        return classmethod(workerfunc)
-    return decoratorfunc
-
-@decorator
-def transwrap(f, *args, **kwargs):
-    """
-        decorates a function so that a DB transaction is always committed after
-        the wrapped function returns and also rolls back the transaction if
-        an unhandled exception occurs
-    """
-    try:
-        retval = f(*args, **kwargs)
-        db.sess.commit()
-        return retval
-    except Exception:
-        db.sess.rollback()
-        raise
-
-def cm_transwrap(f):
-    """
-        like transwrap() but makes the function a class method
-    """
-    return classmethod(transwrap(f))
-
-@decorator
-def one_to_none(f, *args, **kwargs):
-    """
-        wraps a function that uses SQLAlahcemy's ORM .one() method and returns
-        None instead of raising an exception if there was no record returned.
-        If multiple records exist, that exception is still raised.
-    """
-    try:
-        return f(*args, **kwargs)
-    except NoResultFound, e:
-        if 'No row was found for one()' != str(e):
-            raise
-        return None
 
 def clear_db():
     if db.engine.dialect.name == 'postgresql':
@@ -147,4 +74,3 @@ def clear_db():
     else:
         return False
     return True
-
