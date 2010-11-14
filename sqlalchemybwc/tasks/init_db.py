ece4ff554f5cb2512595c78e2fbb103ef85567d8
Randy Syring <rsyring@gmail.com>
2010-11-13 23:19:39 -0500

    **BC BREAK**: changed pre_test_init functionality & settings
    There is now more control available over what db related tasks are run
    prior to tests being ran.  The only thing this should really affect is
    migration related testing.

--- sqlalchemybwc/tasks/init_db.py
+++ sqlalchemybwc/tasks/init_db.py
@@ -3,8 +3,8 @@ from sqlitefktg4sa import auto_assign
 
 def action_10_create_db_objects():
     """ initialize the database """
-    # create foreign keys for SQLite
+    # create foreign key triggers for SQLite
     auto_assign(db.meta, db.engine)
-    
+
     # create the database objects
     db.meta.create_all(bind=db.engine)
