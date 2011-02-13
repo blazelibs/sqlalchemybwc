6d1d35a67293d3f8ef76635dc07495af22548c1b
Randy Syring <randy.syring@level12.io>
2011-02-13 17:15:02 -0500

    add code to middleware.py to help with debugging, no public API

--- sqlalchemybwc/lib/middleware.py
+++ sqlalchemybwc/lib/middleware.py
@@ -4,18 +4,63 @@ from blazeweb.utils import registry_has_object
 from paste.registry import StackedObjectProxy
 from savalidation import ValidatingSessionExtension
 from sqlalchemy import engine_from_config, MetaData
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session, Session
 
 db = StackedObjectProxy(name="db")
+
+class DebugSession(Session):
+    """
+        This was helpful in troubleshooting a deadlock condition that
+        occured during testing.
+    """
+    def __init__(self, *args, **kwargs):
+        self.dbg_label = kwargs.pop('dbg_label')
+        Session.__init__(self, *args, **kwargs)
+
+    def execute(self, clause, params=None, mapper=None, **kw):
+        try:
+            print 'E>>> ' + self.dbg_label
+            return Session.execute(self, clause, params, mapper, **kw)
+        finally:
+            print '<<<E'
+
+    def _flush(self, objects=None):
+        try:
+            print '>>>F ' + self.dbg_label
+            return Session._flush(self, objects)
+        finally:
+            print '<<<F'
+
+    def commit(self):
+        try:
+            print '>>>C ' + self.dbg_label
+            return Session.commit(self)
+        finally:
+            print '<<<C'
 
 class SQLAlchemyContainer(object):
 
     def __init__(self):
         self.engine = engine_from_config(dict(settings.db), prefix='')
         self.meta = MetaData()
-        self.Session = scoped_session(sessionmaker(bind=self.engine, extension=ValidatingSessionExtension()))
+        self.Session = self.make_session()
         if settings.components.sqlalchemy.use_split_sessions:
-            self.AppLevelSession = scoped_session(sessionmaker(bind=self.engine, extension=ValidatingSessionExtension()))
+            self.AppLevelSession = self.make_session()
+
+    def make_session(self, dbg_label = None):
+        sm_kwargs = {
+            'bind': self.engine,
+            'extension': ValidatingSessionExtension(),
+        }
+        if dbg_label:
+            sess_inst = sessionmaker(
+                class_ = DebugSession,
+                dbg_label = dbg_label,
+                **sm_kwargs
+            )
+        else:
+            sess_inst = sessionmaker(**sm_kwargs)
+        return scoped_session(sess_inst)
 
     def get_scoped_session_class(self):
         if settings.components.sqlalchemy.use_split_sessions and not registry_has_object(rg):
