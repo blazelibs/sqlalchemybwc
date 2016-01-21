6736fa76f635ee5279d9d06b6d3e06e9070592b3
Bill Adams <bladams@users.noreply.github.com>
2016-01-21 14:38:53 -0500

    support formencode >= 1.3

--- sqlalchemybwc/lib/validators.py
+++ sqlalchemybwc/lib/validators.py
@@ -5,7 +5,21 @@ from sqlalchemy import sql as sasql
 
 from compstack.sqlalchemy import db
 
-class _UniqueValidator(formencode.validators.FancyValidator):
+
+class BaseValidator(formencode.validators.FancyValidator):
+    def __classinit__(cls, new_attrs):
+        depricated_methods = getattr(cls, '_deprecated_methods', None) or \
+            new_attrs.get('_deprecated_methods')
+        if depricated_methods is not None:
+            for old, new in depricated_methods:
+                if old in new_attrs:
+                    method = new_attrs.pop(old)
+                    setattr(cls, new, method)
+                    new_attrs[new] = method
+        return formencode.validators.FancyValidator.__classinit__(cls, new_attrs)
+
+
+class _UniqueValidator(BaseValidator):
     """
     Calls the given callable with the value of the field.  If the return value
     does not evaluate to false, Invalid is raised
